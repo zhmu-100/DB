599034367eae82c784d7d7f2624946cb2472b818
graevsky <78727984+graevsky@users.noreply.github.com>
2025-05-02 18:42:34 +0300

    Revert "add unit tests"

--- src/main/kotlin/com/db/orm/connection/DatabaseConfig.kt
+++ src/main/kotlin/com/db/orm/connection/DatabaseConfig.kt
@@ -1,5 +1,19 @@
-import io.github.cdimascio.dotenv.Dotenv
+package com.db.orm.connection
 
+import io.github.cdimascio.dotenv.dotenv
+
+/**
+ * Конфигурация для подключения к базе данных PostgreSQL.
+ *
+ * Содержит следующие параметры:
+ * - [host]: адрес сервера базы данных.
+ * - [port]: порт сервера базы данных.
+ * - [database]: имя базы данных.
+ * - [user]: имя пользователя для подключения.
+ * - [password]: пароль для подключения.
+ *
+ * Метод [load] загружает значения из переменных окружения с использованием библиотеки dotenv.
+ */
 data class DatabaseConfig(
     val host: String,
     val port: Int,
@@ -8,17 +22,26 @@ data class DatabaseConfig(
     val password: String
 ) {
   companion object {
+    /**
+     * Загружает конфигурацию из переменных окружения.
+     *
+     * - DB_HOST (по умолчанию: "localhost")
+     * - DB_PORT (по умолчанию: 5432)
+     * - DB_NAME (по умолчанию: postgres, idk)
+     * - DB_USER (по умолчанию: postgres)
+     * - DB_PASSWORD (по умолчанию: пустая строка)
+     *
+     * @return Экземпляр [DatabaseConfig] с загруженными параметрами.
+     */
     fun load(): DatabaseConfig {
-      return loadCustom(Dotenv.configure().load())
-    }
+      val dotenv = dotenv()
 
-    internal fun loadCustom(env: Dotenv): DatabaseConfig {
       return DatabaseConfig(
-          host = env["DB_HOST"] ?: "localhost",
-          port = env["DB_PORT"]?.toIntOrNull() ?: 5432,
-          database = env["DB_NAME"] ?: "postgres",
-          user = env["DB_USER"] ?: "postgres",
-          password = env["DB_PASSWORD"] ?: "")
+          host = dotenv["DB_HOST"] ?: "localhost",
+          port = dotenv["DB_PORT"]?.toIntOrNull() ?: 5432,
+          database = dotenv["DB_NAME"] ?: "postgres",
+          user = dotenv["DB_USER"] ?: "postgres",
+          password = dotenv["DB_PASSWORD"] ?: "")
     }
   }
 }
